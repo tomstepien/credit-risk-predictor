c8c742c2838a299acdb5d58844f4098c0895b94e
tstepien <tstepien101@gmail.com>
2026-04-11 01:25:09 +0200

    Add SMOTE/RUS imbalance handling notebook and refactor preprocessing

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -1,27 +1,24 @@
 import pandas as pd
 import numpy as np
-
-def clean_age(df) -> pd.DataFrame:
-    df.loc[df['age'] == 0, 'age'] = df['age'].median()
+def fill_missing_values(df) -> pd.DataFrame:
+    df['NumberOfDependents'] = df['NumberOfDependents'].fillna(0).astype(np.int64)
     return df
 
-def fill_missing_values(df) -> pd.DataFrame:
-    # Filling NumberOfDependents
-    df['NumberOfDependents'] = df['NumberOfDependents'].fillna(0).astype(np.int64)
+def fit_preprocess(df) -> tuple[int, float, float]:
+    median_age = df['age'].median()
 
-    # Filling MonthlyIncome
-    working = df[(df['age'] >= 18) & (df['age'] < 60)]
-    senior = df[df['age'] >= 60]
+    working_mean_income = df[(df['age'] < 60)]['MonthlyIncome'].mean()
+    senior_mean_income = df[(df['age'] >= 60)]['MonthlyIncome'].mean()
 
+    return median_age, working_mean_income, senior_mean_income
+
+def transform(df, median_age, working_mean, senior_mean) -> pd.DataFrame:
+    df = df.copy()
+
+    df.loc[df['age'] == 0, 'age'] = median_age
     df['MonthlyIncome'] = df['MonthlyIncome'].fillna(-1)
 
-    df.loc[(df['MonthlyIncome'] == -1) & (df['age'] < 60), 'MonthlyIncome'] = working['MonthlyIncome'].mean()
-    df.loc[(df['MonthlyIncome'] == -1) & (df['age'] >= 60), 'MonthlyIncome'] = senior['MonthlyIncome'].mean()
+    df.loc[(df['MonthlyIncome'] == -1) & (df['age'] < 60), 'MonthlyIncome'] = working_mean
+    df.loc[(df['MonthlyIncome'] == -1) & (df['age'] >= 60), 'MonthlyIncome'] = senior_mean
 
-    return df
-
-def preprocess(df) -> pd.DataFrame:
-    df = df.copy()
-    df = clean_age(df)
-    df = fill_missing_values(df)
     return df
